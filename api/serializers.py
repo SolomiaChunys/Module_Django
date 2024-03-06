cf3c45c1a2ccb0b2e4bc8db9768318d812b92257
Rosemia <solomijka.zet@gmail.com>
2024-03-06 14:06:57 +0200

    task 2

---
+++ api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from shop.models import User, Product, Order
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'password',
+            'wallet'
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'description',
+            'price',
+            'count'
+        ]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'user',
+            'product',
+            'count'
+        ]
