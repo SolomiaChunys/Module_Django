474c766e233fcd65876d70b0ce79e39468be3525
Rosemia <solomijka.zet@gmail.com>
2024-03-30 00:07:27 +0200

    task 2

--- api/async.py
+++ api/async.py
@@ -3,35 +3,27 @@ import asyncio
 from time import time
 
 
-async def get_page(session, url):
-    async with session.get(url) as resp:
-        return await resp.text()
+async def power_numb(numb):
+    return numb ** 1000000
 
 
-async def main(urls):
+async def main(numbs):
     start = time()
+    tasks = []
 
-    async with aiohttp.ClientSession() as session:
-        tasks = []
+    for numb in numbs:
+        task = asyncio.create_task(power_numb(numb))
+        tasks.append(task)
 
-        for url in urls:
-            for _ in range(5):
-                task = asyncio.create_task(get_page(session, url))
-                tasks.append(task)
-
-        await asyncio.gather(*tasks)
+    await asyncio.gather(*tasks)
 
     end = time()
     return f'Async request time: {end - start}'
 
 
 if __name__ == "__main__":
-    urls = [
-        'https://google.com',
-        'https://amazon.com',
-        'https://microsoft.com'
-    ]
-    print(asyncio.run(main(urls)))
+    numbs = [2, 3, 5]
+    print(asyncio.run(main(numbs)))
 
 
-# Async request time: 1.9171
+# Async request time: 0.1999
