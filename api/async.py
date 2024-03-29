9a1aa72973a279aba975bf0ade28bdea3b568274
Rosemia <solomijka.zet@gmail.com>
2024-03-29 23:30:31 +0200

    task 1

---
+++ api/async.py
@@ -0,0 +1,36 @@
+import aiohttp
+import asyncio
+from time import time
+
+
+async def get_page(session, url):
+    async with session.get(url) as resp:
+        return await resp.text()
+
+
+async def main(urls):
+    start = time()
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        for url in urls:
+            task = asyncio.create_task(get_page(session, url))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+    end = time()
+    return f'Async request time: {end - start}'
+
+
+if __name__ == "__main__":
+    urls = [
+        'https://google.com',
+        'https://amazon.com',
+        'https://microsoft.com'
+    ]
+    print(asyncio.run(main(urls)))
+
+
+# Async request time: 2.1010
